8ec079e435eb62ab385306036d299c2eb8725d57
mohamed khaled <76697066+mohamedkhaled123447@users.noreply.github.com>
2025-04-04 16:16:20 +0200

    Add files via upload

--- ECommerceSystem.java
+++ ECommerceSystem.java
@@ -7,7 +7,7 @@ public class ECommerceSystem {
     public static void main(String[] args) {
 
         Product cheese = new ShippableExpirableProduct("cheese", 8.99, 10,
-                LocalDate.of(2000,12,22), 0.2);
+                LocalDate.now().plusDays(14), 0.2);
         Product tv = new ShippableProduct("Smart TV", 799.99, 5, 12.5);
         Product card = new NormalProduct("card", 10.0, 100);
 
@@ -17,13 +17,22 @@ public class ECommerceSystem {
         ShippingStrategy shippingService = new StandardShipping();
         Customer customer = new Customer(cart, visa, shippingService, 1500);
 
-
+       //normal case
         customer.getCart().addItem(cheese, 10);
         customer.getCart().addItem(tv, 1);
         customer.getCart().addItem(card, 3);
         customer.checkout();
-
-
+        System.out.println("\n*******************************************\n");
+        //handle date expire
+       /* Product cheese1 =new ShippableExpirableProduct("cheese1", 8.99, 10,
+                LocalDate.of(2024,2,2), 0.2);
+        customer.getCart().addItem(cheese1,2);
+        customer.checkout();
+        System.out.println("\n*******************************************\n");*/
+        // handle Out of stock
+       /* Product phone = new ShippableProduct("Smart phone", 799.99, 5, 12.5);
+        customer.getCart().addItem(phone,10);
+        customer.checkout();*/
     }
 
 }
